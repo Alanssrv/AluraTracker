1b60594adebe9c6de9393176683b41f928fbad59
Alanssrv <alanssrv@gmail.com>
2023-09-27 21:24:08 -0300

    adicionando funcionalidades ao cronometro organizacao de componentes

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -2,26 +2,10 @@
     <div class="box">
         <div class="columns">
             <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
-                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
+                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" v-model="descricao">
             </div>
             <div class="column">
-                <div class="is-flex is-align-items-center is-justify-content-space-between">
-                    <section>
-                        <strong>{{tempoDecorrido}}</strong>
-                    </section>
-                    <button class="button" @click="iniciarCronometro">
-                        <span class="icon">
-                            <i class="fas fa-play"></i>
-                        </span>
-                        <span>play</span>
-                    </button>
-                    <button class="button" @click="finalizarCronometro">
-                        <span class="icon">
-                            <i class="fas fa-stop"></i>
-                        </span>
-                        <span>stop</span>
-                    </button>
-                </div>
+                <Temporizador @ao-finalizar-cronometro="finalizarTarefa"/>
             </div>
         </div>
     </div>
@@ -29,29 +13,24 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Temporizador from "./Temporizador.vue";
 
 export default defineComponent({
     name: 'Formulario',
+    components: {
+        Temporizador
+    },
     data () {
         return {
-            tempoEmSegundos: 0,
-            cronometro: 0
-        }
-    },
-    computed: {
-        tempoDecorrido () : string {
-            return new Date(this.tempoEmSegundos * 1000).toISOString().substring(11, 19);
-        }
+            descricao: ''
+        };
     },
     methods: {
-        iniciarCronometro() {
-            this.cronometro = setInterval(() => {
-                this.tempoEmSegundos++;
-            }, 1000);
-        },
-        finalizarCronometro() {
-            clearInterval(this.cronometro);
-            this.tempoEmSegundos = 0;
+        finalizarTarefa(tempoEmSegundos : number) : void {
+            console.log(tempoEmSegundos);
+            console.log(this.descricao);
+            // enviar para o próximo
+            this.descricao = '';
         }
     }
 });
---
+++ src/components/Temporizador.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="is-flex is-align-items-center is-justify-content-space-between">
+        <Cronometro :tempo-em-segundos="tempoEmSegundos"/>
+        <Botao
+            :icone="'fas fa-play'"
+            :texto="'play'"
+            :desabilitado="cronometroRodando"
+            @clicado="iniciarCronometro"
+        />
+        <Botao
+            :icone="'fas fa-stop'"
+            :texto="'stop'"
+            :desabilitado="!cronometroRodando"
+            @clicado="finalizarCronometro"
+        />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Cronometro from "./Cronometro.vue";
+import Botao from "./Botao.vue";
+
+export default defineComponent({
+    name: 'Temporizador',
+    data () {
+        return {
+            tempoEmSegundos: 0,
+            cronometro: 0,
+            cronometroRodando: false
+        }
+    },
+    emits: ['aoFinalizarCronometro'],
+    components: {
+        Cronometro,
+        Botao
+    },
+    methods: {
+        iniciarCronometro() {
+            this.cronometroRodando = true;
+            this.cronometro = setInterval(() => {
+                this.tempoEmSegundos++;
+            }, 1000);
+        },
+        finalizarCronometro() {
+            this.cronometroRodando = false;
+            clearInterval(this.cronometro);
+            this.$emit('aoFinalizarCronometro', this.tempoEmSegundos);
+            this.tempoEmSegundos = 0;
+        }
+    },
+});
+</script>
+
+<style scoped>
+
+</style>
